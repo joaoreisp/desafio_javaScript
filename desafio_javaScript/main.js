89eb2f70f754d835d16805bf4ceecb5316978577
joaoreisp <148827205+joaoreisp@users.noreply.github.com>
2024-08-07 00:06:08 -0300

    Add files via upload

---
+++ desafio_javaScript/main.js
@@ -0,0 +1,75 @@
+// 1)
+// alert("Hello World!")
+
+
+// 2)
+// const numberOne = 15
+// const numberTwo = 5
+// const sum = numberOne + numberTwo
+// alert(sum)
+
+
+// 3)
+// const isNumber = 10
+// if (typeof isNumber == 'number') {
+//     alert('É um número')
+// } else {
+//     alert('Não é um número')
+// }
+
+
+// 4)
+// const isString = "Olá"
+// if (typeof isString == 'string') {
+//     alert('É uma string')
+// } else {
+//     alert('Não é uma string')
+// }
+
+
+// 5)
+// const isBoolean = true
+// if (typeof isBoolean == 'boolean') {
+//     alert('É um booleano')
+// } else {
+//     alert('Não é um booleano')
+// }
+
+
+// 6)
+// const numberOne = 10
+// const numberTwo = 5
+// const sub = numberOne - numberTwo
+// alert(sub)
+
+
+// 7)
+// const numberOne = 10
+// const numberTwo = 5
+// const multi = numberOne * numberTwo
+// alert(multi)
+
+
+// 8)
+// const numberOne = 10
+// const numberTwo = 5
+// const div = numberOne / numberTwo
+// alert(div)
+
+
+// 9)
+// const number = 50
+// if (number % 2 == 0) {
+//     alert('É um número par')
+// } else {
+//     alert('Não é um número par')
+// }
+
+
+// 10)
+// const number = 5
+// if (number % 2 != 0) {
+//     alert('É um número ímpar')
+// } else {
+//     alert('Não é um número ímpar')
+// }
